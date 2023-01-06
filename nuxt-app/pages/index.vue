7b28db7c529be4233342d79f724992f68d8e2b7e
kosukenishio2 <rimeoftheancientmariner0415@yahoo.co.jp>
2023-01-07 01:02:10 +0900

    new

---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p class="text-black font-bold ml-20">
+      西尾洪介・それは人間
+    </p>
+    <p class="text-black font-bold my-10 ml-24">
+      詩人　ニューシュールレアリスト　でいてYOUTUBER
+    </p>
+    <p class="text-yellow-500 font-bold">
+  
+    </p>
+    <p class="text-pink-500 font-bold">
+    
+    </p>
+    <p class="text-black font-bold">
+     西尾とは何ぞや
+    </p>
+宿主のいない蜘蛛の巣に引っかかった神父
+
+    <h1 class="text-sm font-bold underline">
+  西尾洪介「人」[]
+!
+</h1>
+
+
+    <p>
+      <a href="/info">西尾への仕事依頼はこちら
+        「このホームページにたどり着いた君はすでに選ばれた、さあ、どんな仕事でも依頼してきなさい」
+        にしお
+      </a>
+    </p>
+
+  </div>
+</template>
---
+++ nuxt-app/pages/info.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <p class="text-black font-bold">
+        インフォです
+      </p>
+      <p class="text-black font-bold my-10 ml-24">
+        インフォです
+      </p>
+      <p class="text-yellow-500 font-bold">
+        インフォです
+      </p>
+      <p class="text-pink-500 font-bold">
+        インフォです
+      </p>
+      <p class="text-black font-bold">
+        うんこうんこうんこうんこうんこうんこうんこうんこ
+      </p>
+  
+  
+      <p>
+        <a href="/info">インフォ</a>
+      </p>
+  
+    </div>
+  </template>
+  
+  
+  
+  
+  
