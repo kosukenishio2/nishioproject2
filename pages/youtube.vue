ec5e58728e5a4f74b062f5607f1a12c7a3aa6f6a
kosukenishio2 <rimeoftheancientmariner0415@yahoo.co.jp>
2023-08-16 18:30:17 +0900

    a

---
+++ pages/youtube.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="bg-white py-6 sm:py-8 lg:py-12">
+  <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+    <div class="flex justify-between items-end gap-4 mb-6">
+      <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold">Collections</h2>
+
+      <a href="#" class="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3">Show more</a>
+    </div>
+
+    <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 md:gap-x-6 gap-y-6">
+      <!-- product - start -->
+      <div>
+        <a href="#" class="group h-96 block bg-gray-100 rounded-lg overflow-hidden shadow-lg mb-2 lg:mb-3">
+          <img src="https://images.unsplash.com/photo-1552374196-1ab2a1c593e8?auto=format&q=75&fit=crop&crop=top&w=600&h=700" loading="lazy" alt="Photo by Austin Wade" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+        </a>
+
+        <div class="flex flex-col">
+          <span class="text-gray-500">Men</span>
+          <a href="#" class="text-gray-800 hover:text-gray-500 text-lg lg:text-xl font-bold transition duration-100">Business Causual</a>
+        </div>
+      </div>
+      <!-- product - end -->
+
+      <!-- product - start -->
+      <div>
+        <a href="#" class="group h-96 block bg-gray-100 rounded-lg overflow-hidden shadow-lg mb-2 lg:mb-3">
+          <img src="/shop/item_01.jpg" loading="lazy" alt="Photo by engin akyurt" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+        </a>
+
+        <div class="flex flex-col">
+          <span class="text-gray-500">Women</span>
+          <a href="#" class="text-gray-800 hover:text-gray-500 text-lg lg:text-xl font-bold transition duration-100">Summer Season</a>
+        </div>
+      </div>
+      <!-- product - end -->
+
+      <!-- product - start -->
+      <div>
+        <a href="#" class="group h-96 block bg-gray-100 rounded-lg overflow-hidden shadow-lg mb-2 lg:mb-3">
+          <img src="https://images.unsplash.com/photo-1552668693-d0738e00eca8?auto=format&q=75&fit=crop&crop=top&w=600&h=700" loading="lazy" alt="Photo by Austin Wade" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+        </a>
+
+        <div class="flex flex-col">
+          <span class="text-gray-500">Men</span>
+          <a href="#" class="text-gray-800 hover:text-gray-500 text-lg lg:text-xl font-bold transition duration-100">Streetwear</a>
+        </div>
+      </div>
+      <!-- product - end -->
+
+      <!-- product - start -->
+      <div>
+        <a href="#" class="group h-96 block bg-gray-100 rounded-lg overflow-hidden shadow-lg mb-2 lg:mb-3">
+          <img src="https://images.unsplash.com/photo-1560269999-cef6ebd23ad3?auto=format&q=75&fit=crop&w=600&h=700" loading="lazy" alt="Photo by Austin Wade" class="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200" />
+        </a>
+
+        <div class="flex flex-col">
+          <span class="text-gray-500">Women</span>
+          <a href="#" class="text-gray-800 hover:text-gray-500 text-lg lg:text-xl font-bold transition duration-100">Sale</a>
+        </div>
+      </div>
+      <!-- product - end -->
+    </div>
+  </div>
+</div>
+</template>
